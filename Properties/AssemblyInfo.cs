c189474af27b1e5d30e9d3e314502f876dce3292
Ondřej Kotas <ok@krtkovo.eu>
2024-09-22 14:23:53 +0200

    remove admin access

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KRtkovo.eu")]
 [assembly: AssemblyProduct("Tetris")]
-[assembly: AssemblyCopyright("made by KRtek™ 2022")]
+[assembly: AssemblyCopyright("made by KRtek™ 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.04")]
-[assembly: AssemblyFileVersion("3.04")]
+[assembly: AssemblyVersion("3.05")]
+[assembly: AssemblyFileVersion("3.05")]
